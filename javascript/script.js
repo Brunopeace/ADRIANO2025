e246747ca0b9230d6871c3966d06953365ba63a4
Brunopeace <137825826+Brunopeace@users.noreply.github.com>
2024-08-16 13:30:56 -0300

    Atualizar o script.js

--- javascript/script.js
+++ javascript/script.js
@@ -881,6 +881,12 @@ function excluirClientesSelecionados() {
         }
     });
 
+    if (clientesExcluidos) {
+        // Tocar o som de exclusão
+        const somExclusao = new Audio('sounds/exclusao.mp3');
+        somExclusao.play();
+    }
+
     salvarClientes(clientes);
     salvarLixeira(lixeira);
     carregarLixeiraPagina();
